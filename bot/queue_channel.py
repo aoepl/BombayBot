24dcaa494f9d461ae9f8418f138b49d9d0e42725
siba rath <siba.rath@thetradedesk.com>
2026-06-01 15:45:51 +0530

    leaderboard

--- bot/queue_channel.py
+++ bot/queue_channel.py
@@ -460,6 +460,7 @@ class QueueChannel:
 
 	async def get_lb(self):
 		now = int(time())
+		inactivity_cutoff = now - 90 * 86400  # drop players with no ranked match in the last 90 days
 		data = await db.select(
 			['user_id', 'nick', 'rating', 'deviation', 'wins', 'losses', 'draws', 'streak', 'is_hidden', 'last_ranked_match_at'],
 			'qc_players',
@@ -469,6 +470,7 @@ class QueueChannel:
 			i for i in data
 			if i['rating'] is not None
 			and not i['is_hidden']
+			and (i['last_ranked_match_at'] or 0) >= inactivity_cutoff
 			and (not self.cfg.lb_last_match_limit or ((i['last_ranked_match_at'] or 0) + self.cfg.lb_last_match_limit > now))
 			and not (self.cfg.lb_min_matches and self.cfg.lb_min_matches > sum((i['wins'], i['losses'], i['draws'])))
 		]
